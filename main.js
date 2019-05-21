5f75c24b310acafd7028e7459e822d6e777ccc0e
AlecVosika <alecvosika@comcast.net>
2019-05-21 04:14:07 -0500

    Added NPC spawning within the program

--- main.js
+++ main.js
@@ -17,10 +17,28 @@ module.exports.loop = function () {
             roleUpgrader.run(creep);
         }
     }
+
+    // _.sum will count the number of properties in Game.creeps filtered by the
+    //  arrow function, which checks for the creep being a harvester
+    var numOfHarvesters = _.sum(Game.creeps, (c) => c.memory.role == 'harvester');
+    // goal: have 10 harvesters and as many upgraders as possible
+    var minNumOfHarvesters = 10;
+    var name = undefined;
+
+    // if not enough harvesters
+    if (numOfHarvesters < minNumOfHarvesters) {
+        // try to spawn one
+        name = Game.spawns.Spawn1.createCreep([WORK,WORK,CARRY,MOVE], 
+            undefined, {role: 'harvester', working: false});
+    }
+    else {
+        // else try to spawn an upgrader
+        name = Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE,MOVE], 
+            undefined, { role: 'upgrader', working: false});
+    }
+
+    // print name to console if spawning was a success
+    if (typeof(name) == "string") {
+        console.log("Spawned new creep: " + name);
+    }
 };
-
-
-//Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE,MOVE],"Alec",{working: false})
-//Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE],"Jeremy",{working: false})
-//Game.creeps.Alec.memory.role = 'harvester'
-//Game.creeps.Jeremy.memory.role = 'upgrader'
